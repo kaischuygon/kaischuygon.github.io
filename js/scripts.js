c78dffb197a94155a71d97848d13bac8764849d4
Kai Schuyler Gonzalez <kai.schuyler@gmail.com>
2021-02-18 20:59:56 +0100

    added theme switching

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+const themeButton = document.getElementById('theme-button')
+const body = document.body
+
+let theme = localStorage.getItem('theme') || 'dark'
+
+themeButton.onclick = () => {
+    console.log('activated')
+    if(theme == 'dark') {
+        localStorage.setItem('theme', 'light')
+        body.classList.replace('dark', 'light')
+        themeButton.innerHTML = '<i class="fas fa-moon"></i>'
+        theme = 'light'
+    } else {
+        localStorage.setItem('theme', 'dark')
+        body.classList.replace('light', 'dark')
+        themeButton.innerHTML = '<i class="fas fa-sun"></i>'
+        theme = 'dark'
+    }
+}
+
+body.onload = () => {
+    body.classList.add(theme)
+    if(theme == 'dark') {
+        themeButton.innerHTML = '<i class="fas fa-sun"></i>'
+    } else {
+        themeButton.innerHTML = '<i class="fas fa-moon"></i>'
+    }
+}
