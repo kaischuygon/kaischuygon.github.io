adeb6bfade1f8496810954a21a2272e5b19602ce
Kai Schuyler Gonzalez <kai.schuyler@gmail.com>
2022-05-20 19:44:33 -0600

    new portfolio images

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,13 @@
+<script>
+import Logo from "./logo.vue";
+export default {
+  Logo
+}
+</script>
+
 <template>
     <div class="bottom-0 w-full flex justify-center items-center my-4">
-        <img src="favicon.ico" class="flex-0 fill-slate-50 h-12" alt="logo">
+        <Logo class="fill-rose-300 h-full"/>
         <div class="flex-0 flex flex-col text-slate-200 mx-4">
             <span>Contact me: <a href="mailto: kai.schuyler@gmail.com" class=" text-rose-300 hover:text-rose-400 border-b-2 border-transparent hover:border-rose-400 duration-300">kai.schuyler@gmail.com</a></span>
             <span>Built using Nuxt and Tailwind</span>
--- components/links.vue
+++ components/links.vue
@@ -1,14 +1,26 @@
+<script>
+import Logo from "./logo.vue";
+export default {
+  Logo
+}
+</script>
+
 <template>
   <div class="fixed top-0 w-full bg-liquid bg-fixed bg-cover">
-    <div class="flex w-full justify-end items-center">
+    <div class="flex justify-end items-center m-4">
+      <a 
+        href="#"
+        class="mr-auto">
+        <Logo class="fill-slate-100 hover:fill-rose-400 h-full duration-300"/>
+      </a>
       <a
-        class="block text-slate-100 hover:text-rose-400 duration-300 m-4 border-b-2 border-transparent hover:border-rose-400"
+        class="text-slate-100 hover:text-rose-400 duration-300 m-4 border-b-2 border-transparent hover:border-rose-400"
         href="./resume.pdf"
         target="_blank"
         >Resume</a
       >
       <a
-        class="block text-slate-100 hover:text-rose-400 duration-300 m-4 border-b-2 border-transparent hover:border-rose-400"
+        class="text-slate-100 hover:text-rose-400 duration-300 m-4 border-b-2 border-transparent hover:border-rose-400"
         href="https://github.com/kaischuygon"
         target="_blank"
         >GitHub</a
