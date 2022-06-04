fdfdd9ff6b279e1ba22f8be9cc1ed9cd6c4de9df
Kai Schuyler Gonzalez <kai.schuyler@gmail.com>
2022-06-03 18:38:10 -0600

    fixes

--- components/Footer.vue
+++ components/Footer.vue
@@ -9,7 +9,7 @@ export default {
     <div class="bottom-0 flex justify-center items-center my-4">
         <Logo class="fill-accent h-full"/>
         <div class="flex-0 flex flex-col mx-4 text-xs md:text-base">
-            <span>Contact me: <a href="mailto: kai.schuyler@gmail.com" class="text-accent hover:text-accent/80 border-b-2 border-transparent hover:border-accent/80 duration-300">kai.schuyler@gmail.com</a></span>
+            <span>Contact me: <a href="mailto: kai.schuyler@gmail.com" class="text-accent border-b-2 border-transparent hover:border-accent duration-300">kai.schuyler@gmail.com</a></span>
             <span>Built using Nuxt and Tailwind</span>
         </div>
     </div>
