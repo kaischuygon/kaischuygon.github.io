bab10aaf7a660ffc24bd501168ad10e5e6150fae
kaischuygon <kai.schuyler@gmail.com>
2023-01-23 20:44:59 -0700

    hotfix

--- components/header.vue
+++ components/header.vue
@@ -6,8 +6,7 @@ import { navbar } from "#components";
 <div class="w-full h-screen bg-fixed bg-cover flex justify-center items-center snap-start">
   <navbar/>
   <div class="flex flex-col md:flex-row justify-center items-center md:h-1/4">
-    <!-- <Logo class="fill-primary w-3/4 mb-2 md:mb-0 md:h-full md:w-auto"/> -->
-    <img src="profile.png" class="w-3/4 mb-2 md:mb-0 md:h-full md:w-auto"/>
+    <img src="profile.png" class="w-3/4 mb-2 md:mb-0 md:h-full md:w-auto rounded"/>
     <div class="mb-4 md:ml-4 md:w-2/4">
       <span class="text-md md:text-lg lg:text-2xl">
         Hello, my name is <br>
